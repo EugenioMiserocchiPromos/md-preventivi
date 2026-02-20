1e84a93e49a633fad36512d2fc853a0eae1c8e4d
EugenioMiserocchiPromos <eugenio@promosrimini.com>
2026-02-20 09:51:48 +0100

    fix: add fonts and update PDF assets/templates

--- resources/views/pdf/frontespizio.blade.php
+++ resources/views/pdf/frontespizio.blade.php
@@ -4,44 +4,100 @@
     <meta charset="utf-8" />
     <title>Preventivo {{ $quote->prot_display }}</title>
     <style>
+      @font-face {
+        font-family: 'Days One';
+        font-style: normal;
+        font-weight: 400;
+        src: url("{{ 'file://' . public_path('fonts/DaysOne/DaysOne-Regular.ttf') }}") format('truetype');
+      }
+      @font-face {
+        font-family: 'Open Sans';
+        font-style: normal;
+        font-weight: 300;
+        src: url("{{ 'file://' . public_path('fonts/OpenSans/OpenSans-Light.ttf') }}") format('truetype');
+      }
+      @font-face {
+        font-family: 'Open Sans';
+        font-style: normal;
+        font-weight: 400;
+        src: url("{{ 'file://' . public_path('fonts/OpenSans/OpenSans-Regular.ttf') }}") format('truetype');
+      }
+      @font-face {
+        font-family: 'Open Sans';
+        font-style: normal;
+        font-weight: 500;
+        src: url("{{ 'file://' . public_path('fonts/OpenSans/OpenSans-Medium.ttf') }}") format('truetype');
+      }
+      @font-face {
+        font-family: 'Open Sans';
+        font-style: normal;
+        font-weight: 700;
+        src: url("{{ 'file://' . public_path('fonts/OpenSans/OpenSans-Bold.ttf') }}") format('truetype');
+      }
+      @font-face {
+        font-family: 'Open Sans';
+        font-style: normal;
+        font-weight: 800;
+        src: url("{{ 'file://' . public_path('fonts/OpenSans/OpenSans-ExtraBold.ttf') }}") format('truetype');
+      }
       @page {
         margin: 36px 40px 24px 40px;
+      }
+      @page :first {
+        margin: 0;
       }
       html,
       body {
         height: 100%;
       }
       body {
-        font-family: DejaVu Sans, sans-serif;
+        font-family: 'Open Sans', sans-serif;
         font-size: 12px;
         color: #0f172a;
         margin: 0;
       }
       h1 {
-        font-size: 26px;
+        font-family: 'Days One', 'Open Sans', sans-serif;
+        font-size: 36px;
         margin: 0 0 14px 0;
         line-height: 1.2;
         text-transform: uppercase;
         color: #b91c1c;
-      }
-      .subtitle {
-        font-size: 12px;
-        text-transform: uppercase;
-        letter-spacing: 1px;
-        color: #475569;
-        margin-bottom: 8px;
       }
       .frontespizio-wrap {
         width: 100%;
         height: 100%;
         border-collapse: collapse;
       }
+      .frontespizio {
+        width: 100%;
+        height: 100%;
+        background-image: url("{{ 'file://' . public_path('pdf/frontespizio-bg.png') }}");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        display: table;
+      }
       .frontespizio-cell {
+        height: 100%;
         vertical-align: middle;
         text-align: center;
       }
+      .page.frontespizio {
+        height: 100%;
+      }
       .frontespizio-content {
-        padding-top: 10%;
+        padding: 40% 10% 0 10%;
+      }
+      .frontespizio-center {
+        height: 100%;
+        width: 100%;
+        display: table;
+      }
+      .frontespizio-center-cell {
+        display: table-cell;
+        vertical-align: middle;
+        text-align: center;
       }
       .frontespizio-box {
         border: 1px solid #babec2;
@@ -51,44 +107,52 @@
         width: 80%;
       }
       .frontespizio-lines {
-        display: flex;
-        justify-content: center;
         text-align: center;
         font-size: 18px;
-        line-height: 1.25;
+        line-height: 1.35;
         word-wrap: break-word;
         margin: 0;
         padding: 0;
       }
+      .frontespizio-line {
+        display: block;
+        margin: 0 0 10px 0;
+      }
+      .frontespizio-line:last-child {
+        margin-bottom: 0;
+      }
       .frontespizio-label {
         color: #b91c1c;
         font-weight: 600;
-        margin-bottom: 50px;
         padding: 0;
         line-height: 1.2em;
-        min-width: 100% !important;
         text-align: center !important;
       }
     </style>
   </head>
   <body>
-    <table class="frontespizio-wrap">
-      <tr>
-        <td class="frontespizio-cell">
-          <div class="frontespizio-content">
-            <div class="subtitle">Preventivo</div>
-            <h1>{{ $quote->title_text }}</h1>
-            <div class="frontespizio-box">
-              <div class="frontespizio-lines prot">
-                <span class="frontespizio-label">PROT:</span> {{ $quote->prot_internal ?? $quote->prot_display }}<br /><br />
-                <span class="frontespizio-label">Cliente:</span> {{ $quote->customer_title_snapshot }}<br /><br />
-                <span class="frontespizio-label">Data:</span> {{ $quote->date }}<br /><br />
-                <span class="frontespizio-label">Cantiere:</span> {{ $quote->cantiere }}
+    <section class="page frontespizio">
+      <table class="frontespizio-wrap">
+        <tr>
+          <td class="frontespizio-cell">
+            <div class="frontespizio-center">
+              <div class="frontespizio-center-cell">
+                <div class="frontespizio-content">
+                  <h1>{{ $quote->title_text }}</h1>
+                  <div class="frontespizio-box">
+                    <div class="frontespizio-lines prot">
+                      <div class="frontespizio-line"><span class="frontespizio-label">PROT:</span> {{ $quote->prot_internal ?? $quote->prot_display }}</div>
+                      <div class="frontespizio-line"><span class="frontespizio-label">Cliente:</span> {{ $quote->customer_title_snapshot }}</div>
+                      <div class="frontespizio-line"><span class="frontespizio-label">Data:</span> {{ $quote->date }}</div>
+                      <div class="frontespizio-line"><span class="frontespizio-label">Cantiere:</span> {{ $quote->cantiere }}</div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </td>
-      </tr>
-    </table>
+          </td>
+        </tr>
+      </table>
+    </section>
   </body>
 </html>
